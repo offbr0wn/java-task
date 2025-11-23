7f65dc54ac9be0449302369356e1a4c535d44ff0
Shashank <shashank201011@gmail.com>
2025-11-23 21:13:27 +0000

    created java project

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//String Exercise
+
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println("Hello and welcome!");
+Scanner inputText = new Scanner(System.in);
 
+while (true) {
+    IO.println("What is your sentence?");
+    String sentence = inputText.nextLine();
+    if (sentence == null || sentence.equals("") || sentence.length() == 0) {
+        IO.println("Invalid sentence!\n");
 
-// This is a comment
-    byte b =120;
-    
-    for (int i = 1; i <= b; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("Number = " + i);
+    }else{
+        IO.println("Original Sentence: " + sentence);
+        IO.println("Substring Sentence: " +sentence.substring(5, sentence.length()-1));
+        IO.println("Uppercase Sentence: " +sentence.toUpperCase());
+        break;
     }
+
+}
+
+
+
+
+
+
 }
